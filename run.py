689ffc49bfa88e37607fcf29476515e9d82a1094
Ramyad Hadidi <rhadidi@gatech.edu>
2015-02-21 23:12:30 -0500

    added script to run

---
+++ run.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+#########################################################################################
+# Author: Ramyad Hadidi (rhadidi@gatech.edu)
+#########################################################################################
+import sys
+import os
+import argparse
+
+#########################################################################################
+# sanity check
+#########################################################################################
+def sanity_check(args):
+    if not args.exe:
+        print "ERROR: Define executables with -e option"
+        exit(0)
+
+    return
+
+#########################################################################################
+# create an argument parser
+#########################################################################################
+def process_options():
+    parser = argparse.ArgumentParser(description='run.py')
+    parser.add_argument("--dryRun", help="Print out the generated script(s) instead of launching jobs", action="store_true", default=False)
+    parser.add_argument("-o", "--outputDir", help="Root directory for result files", default="results")
+    parser.add_argument("-e", "--exe", action='append', nargs='*', help="Executables to run")
+    parser.add_argument("-s", "--submit",  help="Submit Jobs to qsub", action="store_true", default=False)
+
+    return parser
+
+#########################################################################################
+# main function
+#########################################################################################
+def main(argv):
+    #parse arguments
+    parser = process_options()
+    args = parser.parse_args()
+    sanity_check(args)
+    
+    current_dir = os.getcwd()
+    trace_dir = os.path.join(current_dir, 'traces' )
+
+    if not os.path.exists(args.outputDir):
+        os.makedirs(args.outputDir)    
+
+    traces = os.listdir (trace_dir)
+
+    dpc_options = ""    
+
+    for dpc in args.exe[0]:
+        for trace in traces:
+            output_filename = "{}_{}".format(dpc.split('_')[1], trace.split('_')[0])
+            command = "zcat " + os.path.join(trace_dir, trace) + " | " + os.path.join(current_dir, dpc) + dpc_options + " | tee " + os.path.join (current_dir, args.outputDir ,output_filename)
+            if args.dryRun:
+                print command
+            else:
+                if args.submit:
+                    exit()
+                else:
+                    os.system(command)
+
+
+
+
+
+
+#########################################################################################
+# main routine
+#########################################################################################
+if __name__ == '__main__':
+    main(sys.argv)
